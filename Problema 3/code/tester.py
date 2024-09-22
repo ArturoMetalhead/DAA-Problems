210f36fad8ba3572d156a58e35bdbc9ca59ed0f6
ana caballero <133125807+anakarla99@users.noreply.github.com>
2024-09-22 16:37:39 -0400

    Update tester.py

--- Problema 3/code/tester.py
+++ Problema 3/code/tester.py
@@ -1,14 +1,14 @@
 from TestsGenerator import generator1, generator2, generator3
 from Problema3 import first_solution, second_solution, third_solution
 from colorama import Fore
+import time
 
 algorithm = {"1" : first_solution, "2" : second_solution, "3" : third_solution}
 generator = {"1" : generator1, "2" : generator2, "3" : generator3}
 
-print("Solutions:")
+
 print("1: First Aproximation", "2: Second Aproximation", "3: Third Aproximation")
-algo1 = input("Choose the first algorithm \n")
-algo2 = input("Choose the second algorithm \n")
+alg1, alg2 = input("Put the 2 algorithm to compare \n").split()
 
 gen = input("Choose the generator algorithm \n")
 
@@ -16,16 +16,24 @@ test_cases_number = int(input("How many test cases you want to generate? \n"))
 
 test_cases = generator[gen](test_cases_number)
 
-if algo1 == algo2:
+if alg1 == alg2:
     for case in test_cases:
-        print(algorithm[algo1](case[0], case[1], case[2]))
+        print(f"P: {case[0]}, R: {case[1]}, k: {case[2]}")
+        start_time = time.time()
+        print(algorithm[alg1](case[0], case[1], case[2]))
+        end_time = time.time()
+        print(f'time case : {end_time - start_time}')
 
 else:
     wrong_cases = 0
 
     for case in test_cases:
-        answer1 = algorithm[algo1](case[0], case[1], case[2])
-        answer2 = algorithm[algo2](case[0], case[1], case[2])
+        start_time1 = time.time()
+        answer1 = algorithm[alg1](case[0], case[1], case[2])
+        end_time1 = time.time()
+        start_time2 = time.time()
+        answer2 = algorithm[alg2](case[0], case[1], case[2])
+        end_time2 = time.time()
         match = answer1 == answer2
         print(f"P: {case[0]}, R: {case[1]}, k: {case[2]}")
 
@@ -37,5 +45,8 @@ else:
             wrong_cases += 1
             print(Fore.RED, f"answer1:{answer1}, answer2:{answer2}, bad")
             print(Fore.RESET, "\n")
+
+        print(f'time case 1: {end_time1 - start_time1}')
+        print(f'time case 2: {end_time2 - start_time2}')
 
     print(f"Number of wrong cases: {wrong_cases} \n")
